10ff951bb9b2969d6e5bdcf0115fb0c2337a426d
sue <sue@suedeMacBook.local>
2015-08-27 20:30:32 +0800

    prepare meiriyida.class.php

--- class/meiriyida.class.php
+++ class/meiriyida.class.php
@@ -8,15 +8,17 @@ class Meiriyida
     public function responseMsg($username) 
     { 
     
-include_once("conn.php");
+		include_once("conn.php");//导入连接文件
 
-		$my_t=getdate(date("U"));
+		$my_t=getdate(date("U"));//获取日期信息
+		//匹配day表格获取存储的日期参数
 		$day_clear = mysql_fetch_array(mysql_query("select * from day where user like 'day'"));
+		//判断日期是否匹配 不然的话更新日期并清空数据，以便继续记入数据。目前为day的参数
 		if ($day_clear['date'] != $my_t[mday]){
 		    $updata_day = mysql_query("UPDATE day SET date='$my_t[mday]' WHERE user='day'");//更新次数
 		    $clear_chisu =  mysql_query("TRUNCATE TABLE chisu");
 		}           
-
+		//题库的操作 答题的操作＃＃＃＃＃＃＃
     $fromUsername = $username; 
     $time = time();       
           //查询次数及积分，如果均为空则在数据库中初始化                
@@ -38,7 +40,7 @@ include_once("conn.php");
 			$chisu_result =$chisu_result +1;
 			$updata_chisu = mysql_query("UPDATE chisu SET data='$chisu_result',timuid = '$row[0]' WHERE user like '%$fromUsername%'");
 		}
-
+		//题库的操作 答题的操作＃＃＃＃＃＃＃＃
     $resultStr = "请听题：\n".$row['timu']."\n【回复“答案+选项”例如“答案A”获取答案，每天最多1个积分，回答错误不加分！重复获取题目视为答题！】";
     return $resultStr; 
 
