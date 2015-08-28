6690ef0041d4407131a088498abec18cb59c1d44
sue's mackbook <sueflybaby@gmail.com>
2015-08-28 11:16:27 +0800

    modify replay.class fix feature:not find the keyword in table of reply

--- class/reply.class.php
+++ class/reply.class.php
@@ -8,8 +8,14 @@ class Reply
         $postStr0 = "select * from reply WHERE z1 like '%$keyword%'";//查询语句
         $sql = mysql_query($postStr0);//获取数组类型结果
         while($row = mysql_fetch_array($sql)){
-		          $data .= $row['z2'];
-		     }  
+        	if (!empty($row)) {
+        		# code...
+        		$data .= $row['z2'];
+        	}else{
+        		$data .="未找到匹配的信息！";
+        	}
+		     
+		}  
 		return $data;
 	}
 }
