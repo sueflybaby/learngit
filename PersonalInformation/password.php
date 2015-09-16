7fa936ef373d7e313454d9aa98ae22bbd6f8e9a1
sue'window <sueflybaby@gmail.com>
2015-09-16 11:06:08 +0800

    add password_change

--- PersonalInformation/password.php
+++ PersonalInformation/password.php
@@ -4,7 +4,7 @@ $user =  trim($_POST['name']);//接受传递的name 如果同名则不行 需要
 $old_password = trim($_POST['oldpassword']);//接受传递的secret
 $new_password = trim($_POST['newpassword']);//接受传递的secret
 
-if(isset($user)>0){
+if(isset($user) && isset($old_password) &&isset($new_password)){
 
   include("conn.php");
 
@@ -24,28 +24,34 @@ if(isset($user)>0){
 			if($update_password){
 				echo '<script language="javascript">
    					 alert("修改成功！请登入。");
-    				 window.location.href="login.html?action=longin";
+    				 window.location.href="login.html?action=login";
 			</script>';
 				exit();
 			}else {
 				echo '<script language="javascript">
    					 alert("修改失败，请重试或者联系管理员！");
-    				 window.location.href="login.html?action=longin";
+    				 window.location.href="login.html?action=login";
 			</script>';
 			}
 		}else{
 			echo '<script language="javascript">
    					 alert("用户名及原始密码错误！");
-    				 window.location.href="login.html?action=longin";
+    				 window.location.href="login.html?action=login";
 			</script>';
 		}
 	}else{
 		echo '<script language="javascript">
    					 alert("查无此人，请重试或者联系管理员！");
-    				 window.location.href="login.html?action=longin";
+    				 window.location.href="login.html?action=login";
 			</script>';
 	}
 
 	$mysqli->close();
+}else{
+	echo '<script language="javascript">
+   					 alert("不能为空！");
+    				 window.location.href="login.html?action=login";
+			</script>';
 }
+
 ?>
